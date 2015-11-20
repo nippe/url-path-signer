e29ec1bcb24a5448b543b8360c6d83330efc72ca
Niklas Nihlén <niklas.nihlen@acast.com>
2015-11-20 22:23:30 +0100

    Added url building method
    and added test-script to package.json

--- test/url-path-signer.js
+++ test/url-path-signer.js
@@ -30,8 +30,36 @@ describe('Signing and verifying URLs', function() {
     sign1.should.be.equal(sign2);
     done();
   });
+
+
+  it('should be the same signgare with both http and https', function(done){
+    var urlSigner = UrlSigner();
+    var httpSign = urlSigner.signature('http://hardcoded.se/index.html', 'my-secret');
+    var httpsSign = urlSigner.signature('https://hardcoded.se/index.html?foo=bar&hey=ya', 'my-secret');
+
+    httpSign.should.equal(httpsSign);
+    done();
+  });
 });
 
+describe('building extrnal URLs', function(done){
+  it('should build the url with a query string param containg the signed URL', function(done){
+    var wantedResult = 'https://imagecdn.acast.com/image?source=https%3A%2F%2Facastprod.blob.core.windows.net%3A443%2Fmedia%2Fv1%2Fd94d9795-bf58-4bbf-8102-e178b9ae60ab%2F-standoutpodcastgs-ih2h5unu.jpg&sign=b85ce72865bb0000eb12903d18d3dd8db52ad771';
+    var originalUrl = 'https://acastprod.blob.core.windows.net:443/media/v1/d94d9795-bf58-4bbf-8102-e178b9ae60ab/-standoutpodcastgs-ih2h5unu.jpg';
+
+    UrlSigner()
+      .buildUrl({
+        urlToSign: originalUrl,
+        targetUrl: 'https://imagecdn.acast.com/image',
+        appendType: 'querystring',
+        qsUrlParameter: 'source',
+        qsSignatureParameter: 'sign'
+      },
+      'my-secret')
+      .should.equal(wantedResult);
+    done();
+  });
+});
 
 
 // var url = require('url');
